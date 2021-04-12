acb1d299b7a35cdacbb07abea718ce56067a7341
Arturony <dell.5@hotmail.com>
2021-04-11 23:08:10 -0500

    initial commit

---
+++ app-back/routes/offers.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var router = express.Router();
+const Mongolib = require("../db/Mongolib");
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+    Mongolib.getDatabase(db => {
+        Mongolib.findDocuments(db, docs => {
+            res.send(docs);
+        })
+    })
+});
+
+router.post('/', function (req, res, next) {
+    Mongolib.getDatabase(db => {
+        Mongolib.insertOffer(db, req.body);
+    })
+});
+
+module.exports = router;
